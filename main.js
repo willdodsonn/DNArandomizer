125bbb9a315b4152194d9ace7c9cbc1271d56218
Will Dodson <sodlax38@gmail.com>
2023-02-23 14:11:19 -0500

    done

--- main.js
+++ main.js
@@ -6,34 +6,65 @@ const returnRandBase = () => {
 
 // Returns a random single stand of DNA containing 15 bases
 const mockUpStrand = () => {
-  const newStrand = [];
+  const newStrand = '';
   for (let i = 0; i < 15; i++) {
-    newStrand.push(returnRandBase());
+    // newStrand.push(returnRandBase());
+    newStrand += returnRandBase();
   }
   return newStrand;
 };
 
-let currentDna = mockUpStrand();
-const pAeqourFactory = (number, array) => {
+const pAequorFactory = (specimenNum, dna) => {
   return {
-    specimenNum: number,
-    dna: array,
+    specimenNum: specimenNum,
+    dna: dna,
     mutate() {
-      let randomIndex = Math.floor(Math.random() * this.dna.length);
-      let randBase = returnRandBase();
-
-      if(this.dna[randomIndex] === randBase){
-        randBase = returnRandBase()
-      }
-      this.dna[randomIndex] = randBase
-      return this.dna 
+      // const formerDnaArray = ['A', 'T', 'C', 'G'];
+      const dnaArray = this.dna.split('');
+      const randomBase = returnRandBase();
+      const randomIndex = Math.floor(Math.random() * dnaArray.length);
+      const randomChar = randomBase[Math.floor(Math.random() * randomBase.length)];
+      dnaArray[randomIndex] = randomChar;
+      this.dna = dnaArray.join('');
+      return this.dna;
     },
-    compareDna(){
-      
+    compareDna(pAequorDna) {
+      let count = 0;
+      for (let i = 0; i < this.dna.length; i++){
+        if(this.dna[i] === pAequorDna.dna[i]){
+          count++;
+        }
+      }
+      const percentDiff = (1 - (count / this.dna.length)) * 100;
+      return percentDiff+'%';
+    },
+    willLikelySurvive(){
+      const elementCount = this.dna.split('').filter(strand => strand === 'C' || strand === 'G').length;
+      const elementPercent = (elementCount / this.dna.length) * 100;
+      return elementPercent >= 60;
     }
   };
 };
 
-console.log(pAeqourFactory(5, mockUpStrand()));
+const pAequorData = [];
 
+while (pAequorData.length < 30){
+  const newData = pAequorFactory(pAequorData.length + 1, returnRandBase());
+  if (newData.willLikelySurvive()) {
+    pAequorData.push(newData);
+  }
+}
+
+console.log(pAequorData);
+
+// const pAequor1 = pAequorFactory(1, 'CGCGCG');
+// const pAequor2 = pAequorFactory(2, 'ACCC');
+// console.log(pAequor1.willLikelySurvive()); 
+
+// const pAequor = pAequorFactory(2, 'ATCG');
+// console.log(pAequor);
+
+// const mutateDna = pAequor.mutate();
+// console.log(mutateDna);
+// console.log(pAequor)
 
